6bb757d4e2dc11ab9d8baaf63e43deb584cc2f7f
MorshedNabil <nabilmorshed1251@gmail.com>
2026-07-12 13:01:55 +0600

    register User moodel into admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from unfold.admin import ModelAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(ModelAdmin, BaseUserAdmin):
+    list_display = ['email', 'first_name', 'last_name', 'role', 'subscription_plan', 'is_verified', 'created_at']
+    list_display_links = ['email', 'first_name', 'last_name', 'role', 'subscription_plan', 'is_verified', 'created_at']
+    list_filter = ['role', 'subscription_plan', 'is_verified', 'subscription_end']
+    search_fields = ['email', 'first_name', 'last_name']
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Pixora', {
+            'fields': ('role', 'phone', 'avatar', 'subscription_plan',
+                       'subscription_period', 'subscription_start', 'subscription_end', 'is_verified'),
+        }),
+    )
